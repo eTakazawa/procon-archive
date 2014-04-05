ee00fc126c5bc2ce3a99b34cef5b4f6f0a4b8d2f
tkzw_21 <takazawa621@gmail.com>
2014-04-05 21:02:41 +0900

    [AC] abc006 abc006_1

---
+++ atcoder.jp/abc006/abc006_1/Main.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<cmath>
+#include<vector>
+#include<algorithm>
+#include<cstdio>
+
+using namespace std;
+
+int main(){
+	int a=0,b=0,sum=0,N;
+	cin >> N;
+	
+	if(N%3==0)
+		cout << "YES" << endl; 
+			
+	else cout << "NO" << endl;
+	
+	
+	return 0;
+}
