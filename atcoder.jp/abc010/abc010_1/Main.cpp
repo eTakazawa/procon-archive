cef0014c2147be432c8defecc96b83a269241a33
tkzw_21 <takazawa621@gmail.com>
2014-06-07 21:12:47 +0900

    [AC] abc010 abc010_1

---
+++ atcoder.jp/abc010/abc010_1/Main.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<string>
+#include<algorithm>
+#include<vector>
+#include<cstdio>
+#include<cstdlib>
+#include<functional>
+#include<cmath>
+using namespace std;
+
+int main(){
+	string kare;
+	cin >> kare;
+	cout << kare << "pp" << endl;
+	return 0;
+}
