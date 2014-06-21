4d7c90504a652a080dbb2457ab79ffc754e058b7
tkzw_21 <takazawa621@gmail.com>
2014-06-21 21:31:14 +0900

    [AC] abc011 abc011_1

---
+++ atcoder.jp/abc011/abc011_1/Main.cpp
@@ -0,0 +1,25 @@
+#include<string>
+#include<algorithm>
+#include<vector>
+#include<cstdio>
+#include<cstdlib>
+#include<functional>
+#include<cmath>
+#include<iostream>
+#include<vector>
+#include<cstring>
+#include<climits>
+#include<ctime>
+#define INF INT_MAX
+
+using namespace std;
+
+int d[100][100];
+int main() {
+	int a;
+cin >> a;
+if(a==12)cout << 1 << endl;
+else cout << a+1 << endl;
+
+	return 0;
+}
