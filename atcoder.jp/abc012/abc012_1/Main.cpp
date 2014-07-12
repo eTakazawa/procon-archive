35dcdcf7a0d0829387ab207a1c55728c102819d6
tkzw_21 <takazawa621@gmail.com>
2014-07-12 21:05:18 +0900

    [AC] abc012 abc012_1

---
+++ atcoder.jp/abc012/abc012_1/Main.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<string>
+#include<vector>
+#include<algorithm>
+#include<functional>
+#include<map>
+#include<cstdio>
+using namespace std;
+
+int main(){
+	int a,b;
+	cin >> a >> b;
+	cout << b << ' ' <<  a << endl;
+	return 0;
+}
