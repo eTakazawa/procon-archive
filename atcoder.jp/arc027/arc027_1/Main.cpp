18f8982adfddfbb9983ae3f1f9b932d34b625e9c
tkzw_21 <takazawa621@gmail.com>
2014-08-02 21:09:27 +0900

    [AC] arc027 arc027_1

---
+++ atcoder.jp/arc027/arc027_1/Main.cpp
@@ -0,0 +1,22 @@
+#include <algorithm>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <set>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <cmath>
+#include <cstdio>
+#include <functional>
+using namespace std;
+
+#define FOR(i,s,e) for (int i = int(s); i != int(e); i++)
+#define FORIT(i,c) for (typeof((c).begin()) i = (c).begin(); i != (c).end(); i++)
+#define ISEQ(c) (c).begin(), (c).end()
+
+int main(void) {
+	int h,m;
+	cin>>h>>m;
+	cout << 18*60 - h*60 - m << endl;
+}
