0ad9f85711019e56d7f3af1ccbc745a6b0015133
tkzw_21 <takazawa621@gmail.com>
2014-09-20 21:00:37 +0900

    [AC] code-festival-2014-quala code_festival_qualA_a

---
+++ atcoder.jp/code-festival-2014-quala/code_festival_qualA_a/Main.cpp
@@ -0,0 +1,29 @@
+#include <algorithm>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <set>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <cmath>
+#include <queue>
+#include <stack>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <functional>
+using namespace std;
+
+#define FOR(i,s,e) for (int i = int(s); i != int(e); i++)
+#define FORIT(i,c) for (typeof((c).begin()) i = (c).begin(); i != (c).end(); i++)
+#define ISEQ(c) (c).begin(), (c).end()
+
+//A
+int main(void) {
+	string s;
+	cin >> s;
+	cout << s << "2014" << endl;
+
+	return 0;
+}
