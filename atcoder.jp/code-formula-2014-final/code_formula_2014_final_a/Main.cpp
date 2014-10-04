fd784534b92dc4198f2f37beacda002606f0fc64
tkzw_21 <takazawa621@gmail.com>
2014-10-04 14:01:24 +0900

    [AC] code-formula-2014-final code_formula_2014_final_a

---
+++ atcoder.jp/code-formula-2014-final/code_formula_2014_final_a/Main.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<string>
+#include<vector>
+#include<map>
+#include<queue>
+#include<cmath>
+#include<algorithm>
+#include<stack>
+#include<numeric>
+using namespace std;
+
+int main(void){
+	int a,b;
+	cin >> a >> b;
+
+	cout << a*b << endl;
+
+	return 0;
+}
