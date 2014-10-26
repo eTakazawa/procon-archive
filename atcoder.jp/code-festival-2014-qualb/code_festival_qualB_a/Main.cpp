1172b08a494c4643c5c5a9ae2d2da83ef05b18e3
tkzw_21 <takazawa621@gmail.com>
2014-10-26 21:00:52 +0900

    [AC] code-festival-2014-qualb code_festival_qualB_a

---
+++ atcoder.jp/code-festival-2014-qualb/code_festival_qualB_a/Main.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<string>
+#include<vector>
+#include<map>
+#include<queue>
+#include<cmath>
+#include<algorithm>
+#include<utility>
+using namespace std;
+
+int main(void){
+	int a,b;
+	cin >> a >> b;
+	cout << (a>b?a:b) << endl;
+	return 0;
+}
