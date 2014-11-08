b42fbf36e6ba23c828245122b7e4f9a3d7e3821c
tkzw_21 <takazawa621@gmail.com>
2014-11-08 12:02:03 +0900

    [AC] code-festival-2014-final code_festival_final_a

---
+++ atcoder.jp/code-festival-2014-final/code_festival_final_a/Main.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<cstdio>
+#include<cmath>
+#include<vector>
+#include<map>
+#include<algorithm>
+#include<string>
+#include<utility>
+
+using namespace std;
+
+int main(void){
+	double a;
+	cin >> a;
+	printf("%.10f\n",50/a);
+
+	return 0;
+}
