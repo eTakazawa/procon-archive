48fcfe8c1451686d72f4832f855df764015bbaa2
tkzw_21 <takazawa621@gmail.com>
2014-12-14 12:00:36 +0900

    [AC] code-thanks-festival-2014-b-open code_thanks_festival_14_qualb_a

---
+++ atcoder.jp/code-thanks-festival-2014-b-open/code_thanks_festival_14_qualb_a/Main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+#include <map>
+#include <utility>
+#include <cstdlib>
+#include <vector>
+#include <cstring>
+#include <cstdio>
+#include <cmath>
+#include <algorithm>
+#include <queue>
+#define ll long long
+
+using namespace std;
+
+int main(void){
+    int a,b;
+    cin >> a >> b;
+    cout << max(a ,b) <<endl;
+
+    return 0;
+}
