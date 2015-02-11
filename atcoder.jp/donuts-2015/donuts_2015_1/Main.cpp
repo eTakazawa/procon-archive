fa03a6f67d5b2b59f6a0a30e554a8efa4f07b769
tkzw_21 <takazawa621@gmail.com>
2015-02-11 19:04:18 +0900

    [AC] donuts-2015 donuts_2015_1

---
+++ atcoder.jp/donuts-2015/donuts_2015_1/Main.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+#define INF INT_MAX / 2
+#define MOD 1000000007
+
+using namespace std;
+
+typedef pair<int,int> PII;
+typedef pair<int,pair<int,int>> PIPII;
+typedef long long ll;
+
+int dx[] = {0,1,0,-1};
+int dy[] = {1,0,-1,0};
+
+int main(void){
+	double R,D;
+	cin >> R >> D;
+	printf("%4f\n",R*R*M_PI * 2*D*M_PI);
+
+	return 0;
+}
