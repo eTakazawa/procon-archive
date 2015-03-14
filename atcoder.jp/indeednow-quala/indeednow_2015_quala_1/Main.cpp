8df36187016fe2912ac13d9769ae7f2a65e2c21c
tkzw_21 <takazawa621@gmail.com>
2015-03-15 06:29:53 +0900

    [AC] indeednow-quala indeednow_2015_quala_1

---
+++ atcoder.jp/indeednow-quala/indeednow_2015_quala_1/Main.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef pair<int,int> P;
+typedef pair<int,pair<int,int>> PP;
+typedef long long ll;
+
+const double EPS = 1e-9;
+const int INF = 1e9;
+const int MOD = 1e9+7;
+
+int dy[] = {0,1,0,-1};
+int dx[] = {1,0,-1,0};
+
+int main(void) {
+	string A,B;
+	cin >> A >> B;
+	cout << A.size()*B.size() << endl;
+	
+
+	return 0;
+}
