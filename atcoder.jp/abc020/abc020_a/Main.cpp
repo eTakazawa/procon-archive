0f2517d7b78553af3f101f248865b2ab18b3a721
tkzw_21 <takazawa621@gmail.com>
2015-03-21 21:01:07 +0900

    [AC] abc020 abc020_a

---
+++ atcoder.jp/abc020/abc020_a/Main.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+int main(void) {
+	int q;
+	cin >> q;
+	if(q == 1){
+		cout << "ABC" << endl;
+	}else{
+		cout << "chokudai" << endl;
+	}
+
+	return 0;
+}
