8a65843329d04211025e6d5e9df13a004bd86ea6
tkzw_21 <takazawa621@gmail.com>
2015-03-21 21:04:10 +0900

    [AC] abc020 abc020_b

---
+++ atcoder.jp/abc020/abc020_b/Main.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+int main(void) {
+	string A,B;
+	cin >> A >> B;
+ 	string s = A+B;
+ 	cout << stoi(A+B)*2 << endl;
+	return 0;
+}
