b4460423164734e9c7131702c0a46925778d943f
tkzw_21 <takazawa621@gmail.com>
2015-04-11 21:06:03 +0900

    [AC] abc021 abc021_a

---
+++ atcoder.jp/abc021/abc021_a/Main.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+int main(void) {
+	int n;
+	cin >> n;
+	cout << n << endl;
+	for(int i=0;i<n;i++){
+		cout << 1 << endl;
+	}
+	
+	return 0;
+}
