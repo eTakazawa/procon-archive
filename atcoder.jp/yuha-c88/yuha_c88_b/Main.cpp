8ea381a920580da96c3278183ce01bd55c1b1b1e
tkzw_21 <takazawa621@gmail.com>
2015-08-02 14:29:15 +0900

    [AC] yuha-c88 yuha_c88_b

---
+++ atcoder.jp/yuha-c88/yuha_c88_b/Main.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void){
+	long long n;
+	cin >> n;
+	if(n % 4 == 0){
+		cout << "GO" << endl;
+	}else {
+		cout << "SEN" << endl;
+	}
+	
+    return 0;
+}
