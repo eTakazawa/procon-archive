ae5176ffff02e65167aa94f212c31dc834b055a2
tkzw_21 <takazawa621@gmail.com>
2015-08-29 23:03:53 +0900

    [AC] abc028 abc028_a

---
+++ atcoder.jp/abc028/abc028_a/Main.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+ using namespace std;
+int main(void){
+int n;
+cin >> n;
+if(n <= 59)cout << "Bad" << endl;
+else if(n <= 89)cout << "Good" << endl;
+else if(n <= 99)cout << "Great" << endl;
+else cout << "Perfect" << endl;
+return 0;
+}
