998323218eac778fa9c1987e9cf47288e2c2fddd
tkzw_21 <takazawa621@gmail.com>
2016-02-06 21:18:51 +0900

    [AC] abc033 abc033_b

---
+++ atcoder.jp/abc033/abc033_b/Main.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void){
+	int n;
+	cin >> n;
+	vector<string> s(n);
+	vector<int> p(n);
+
+	int sum = 0;
+	for(int i=0;i<n;i++){
+		cin >> s[i] >> p[i];
+		sum += p[i];
+	}
+
+	for(int i=0;i<n;i++){
+		if( sum/2 <  p[i]){
+			cout << s[i] << endl;
+			return 0;
+		}
+	}
+	cout << "atcoder" << endl;
+	return 0;
+}
