379dbc27f33e79ed25d47e5c7a72353e23e67b37
tkzw_21 <takazawa621@gmail.com>
2016-02-25 15:27:53 +0900

    [AC] jag2013summer-day3 icpc2013summer_day3_d

---
+++ atcoder.jp/jag2013summer-day3/icpc2013summer_day3_d/Main.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void){
+	int n;
+	cin >> n;
+	if( n == 0 )cout << 1 << endl;
+	else if( n == 1 )cout << 2 << endl;
+	else if( n == 2 )cout << 1 << endl;
+	else cout << 0 << endl;
+
+	
+	
+	return 0;
+}
