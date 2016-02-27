19cd9778df432305234eb4495f640b45021d703b
tkzw_21 <takazawa621@gmail.com>
2016-02-27 21:02:13 +0900

    [AC] mujin-pc-2016 mujin_pc_2016_a

---
+++ atcoder.jp/mujin-pc-2016/mujin_pc_2016_a/Main.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void){
+	char c;cin >> c;
+	if(c == 'O' || c == 'P' || c == 'L' || c == 'K'){
+		cout << "Right" << endl;
+	}else{
+		cout << "Left" << endl;
+	}
+
+
+	return 0;
+}
