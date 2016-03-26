4644c37d41d422c29c544da323adc94cf4716412
tkzw_21 <takazawa621@gmail.com>
2016-03-26 21:02:04 +0900

    [AC] abc035 abc035_a

---
+++ atcoder.jp/abc035/abc035_a/Main.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void){
+	int W,H;
+	cin >> W >> H;
+	if(W%16==0 && H%9==0)cout << "16:9" << endl;
+	else cout << "4:3" << endl;
+
+	return 0;
+}
