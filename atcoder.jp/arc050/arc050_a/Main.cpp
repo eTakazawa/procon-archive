59c842602e94f09d628f6b3ec2a3b8140b98d129
tkzw_21 <takazawa621@gmail.com>
2016-04-02 21:02:11 +0900

    [AC] arc050 arc050_a

---
+++ atcoder.jp/arc050/arc050_a/Main.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void){
+	char C,c;
+	cin >> C >> c;
+	if(C + 32 == c)cout << "Yes" << endl;
+	else cout << "No" << endl;
+
+
+	return 0;
+}
