b60405720b84ac1bc6e2d004ed8b9b5cadb1d907
tkzw_21 <takazawa621@gmail.com>
2016-07-16 21:02:32 +0900

    [AC] agc001 agc001_a

---
+++ atcoder.jp/agc001/agc001_a/Main.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void){
+	int N;
+	cin >> N;
+	vector<int> L(2*N);
+	for(int i=0;i<2*N;i++){
+		cin >> L[i];
+	}
+	sort(L.begin(),L.end());
+	int ret = 0;
+	for(int i=0;i<2*N;i+=2){
+		ret += L[i];
+	}
+	cout << ret << endl;
+	return 0;
+}
