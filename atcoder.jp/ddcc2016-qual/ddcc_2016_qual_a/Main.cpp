262bcebbcd8a1bd370ffb3983aae23d2f53b66ea
tkzw_21 <takazawa621@gmail.com>
2016-11-05 21:01:46 +0900

    [AC] ddcc2016-qual ddcc_2016_qual_a

---
+++ atcoder.jp/ddcc2016-qual/ddcc_2016_qual_a/Main.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void){
+	int A,B,C;
+	cin >> A >> B >> C;
+
+	cout << fixed << setprecision(10) << (double)C/A * B << endl;
+	return 0;
+}
