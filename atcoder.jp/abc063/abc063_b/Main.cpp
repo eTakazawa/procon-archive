402f25345d7f66fe2acfd210ff2f6004e63c3b73
tkzw_21 <takazawa621@gmail.com>
2017-06-12 16:48:41 +0900

    [AC] abc063 abc063_b

---
+++ atcoder.jp/abc063/abc063_b/Main.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int a[26];
+int main(void) {
+  string s;
+  cin >> s;
+  for(int i=0;i<s.size();i++){
+    a[s[i] - 'a']++;
+  }
+
+  for(int i=0;i<26;i++){
+    if(a[i] > 1){
+      cout << "no" << endl;
+      return 0;
+    }
+  }
+  cout << "yes" << endl;
+  return 0;
+}
