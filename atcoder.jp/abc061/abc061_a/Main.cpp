a80483db5d88da1c2c7afd04638d2c5d635b0767
tkzw_21 <takazawa621@gmail.com>
2018-05-25 16:45:50 +0900

    [AC] abc061 abc061_a

---
+++ atcoder.jp/abc061/abc061_a/Main.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void) {
+  int a, b, c;
+  cin >> a >> b >> c;
+  if (c >= a && c <= b) cout << "Yes" << endl;
+  else cout << "No" << endl;
+
+  return 0;
+}
