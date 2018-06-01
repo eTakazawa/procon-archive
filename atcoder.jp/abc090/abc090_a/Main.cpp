f03d80b545299cfb2a80f50f842dc106bc9bf963
tkzw_21 <takazawa621@gmail.com>
2018-06-01 16:41:46 +0900

    [AC] abc090 abc090_a

---
+++ atcoder.jp/abc090/abc090_a/Main.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void) {
+  string s;
+  for (int i = 0; i < 3; i++) {
+    cin >> s;
+    cout << s[i];
+  }
+  cout << endl;
+
+  return 0;
+}
