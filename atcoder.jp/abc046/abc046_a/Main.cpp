a99113fa8ec9ade6888940ca9f6b40cdcaf01647
tkzw_21 <takazawa621@gmail.com>
2018-06-08 16:36:13 +0900

    [AC] abc046 abc046_a

---
+++ atcoder.jp/abc046/abc046_a/Main.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void) {
+  int a, b, c;
+  cin >> a >> b >> c;
+  if (a == b && b == c) cout << 1 << endl;
+  else if (a != b && b != c && c != a) cout << 3 << endl;
+  else cout << 2 << endl;
+
+  return 0;
+}
