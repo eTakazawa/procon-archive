6698e92ce8f433be9e16f1b2a2670b060bc05b80
tkzw_21 <takazawa621@gmail.com>
2018-06-10 21:01:12 +0900

    [AC] abc099 abc099_a

---
+++ atcoder.jp/abc099/abc099_a/Main.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void) {
+  int n;
+  cin >> n;
+  if (n > 999) {
+    cout << "ABD" << endl;
+  } else {
+    cout << "ABC" << endl;
+  }
+  return 0;
+}
