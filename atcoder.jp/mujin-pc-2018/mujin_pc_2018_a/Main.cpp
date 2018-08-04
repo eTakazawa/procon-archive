69f662bda2e8ead35617d249b43d58024e7ae678
tkzw_21 <takazawa621@gmail.com>
2018-08-04 21:00:55 +0900

    [AC] mujin-pc-2018 mujin_pc_2018_a

---
+++ atcoder.jp/mujin-pc-2018/mujin_pc_2018_a/Main.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void) {
+  string s;
+  cin >> s;
+  if (s.substr(0, 5) == "MUJIN") cout << "Yes" << endl;
+  else cout << "No" << endl;
+
+  return 0;
+}
