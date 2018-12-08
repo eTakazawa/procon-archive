282e5fd37c967f7a925bf13e4df8d5f1a3bc3cf0
tkzw_21 <takazawa621@gmail.com>
2018-12-08 21:13:01 +0900

    [AC] abc115 abc115_b

---
+++ atcoder.jp/abc115/abc115_b/Main.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void) {
+  int N;
+  cin >> N;
+  vector<int> p(N);
+  for (int i = 0; i < N; i++) {
+    cin >> p[i];
+  }
+  sort(p.begin(), p.end());
+  p[N - 1] /= 2;
+  int sum = 0;
+  for (int i = 0; i < N; i++) {
+    sum += p[i];
+  }
+  cout <<sum << endl;
+
+  return 0;
+}
